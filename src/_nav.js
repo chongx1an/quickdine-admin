8af046a7f467d57cc0b9365a5a162f08e5401539
chongxian <chongxian@easystore.co>
2020-02-06 12:17:44 +0800

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,28 @@
+export default {
+  items: [
+    {
+      name: 'Home',
+      url: '/home',
+      icon: 'icon-home',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      name: 'Orders',
+      url: '/orders',
+      icon: 'icon-list',
+    },
+    {
+      name: 'Products',
+      url: '/products',
+      icon: 'icon-food',
+    },
+    {
+      name: 'Tables',
+      url: '/tables',
+      icon: 'icon-pencil',
+    },
+  ],
+};
