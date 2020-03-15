abb5c5b72ecd71afd39b6cb61c48e236bc92ab70
kkloh98 <40446720+kkloh98@users.noreply.github.com>
2020-03-15 16:13:39 +0800

    done create/edit/list/retrieve product without images

--- src/views/Products/EditProduct/EditProduct.js
+++ src/views/Products/EditProduct/EditProduct.js
@@ -1,7 +1,24 @@
-import React, { Component, useState } from 'react';
-import { Form, FormGroup, Input, Button, Card, CardBody, CardFooter, CardHeader, Col, Row, ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
-import ApiClient from '../../../ApiClient';
-import './styles.css';
+import React, { Component, useState, useEffect } from "react";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Row,
+  ButtonDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle
+} from "reactstrap";
+import ApiClient from "../../../ApiClient";
+import "./styles.css";
+import { ToastContainer, toast } from "react-toastify";
+import LoadingButton from "../../Buttons/LoadingButton";
 
 // sampleProductData = {
 
@@ -32,48 +49,47 @@ import './styles.css';
 
 // }
 
-
 export default props => {
-
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState(0.00);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0.0);
   const [variants, setVariants] = useState([]);
   const [combinations, setCombinations] = useState([]);
   const [showTypeDropdown, setShowTypeDropdown] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const addVariant = () => variants.length < 3 && setVariants([...variants, { type: { name: getOptionType() }, options: [] }]);
+  const addVariant = () =>
+    variants.length < 3 &&
+    setVariants([
+      ...variants,
+      { type: { name: getOptionType() }, options: [] }
+    ]);
 
   const editVariantType = (e, index) => {
-
     var variantsCopy = [...variants];
 
     variantsCopy[index].type = { name: e.target.value };
 
     setVariants(variantsCopy);
+  };
 
-  }
-
-  const removeVariant = variant => variants.length && setVariants(variants.filter(x => x !== variant));
+  const removeVariant = variant =>
+    variants.length && setVariants(variants.filter(x => x !== variant));
 
   const addVariantOption = (e, index) => {
-
-    if(e.target.value !== '') {
-
+    if (e.target.value !== "") {
       var variantsCopy = [...variants];
 
-      !variantsCopy[index].options.includes(e.target.value) && variantsCopy[index].options.push({ name: e.target.value });
+      !variantsCopy[index].options.includes(e.target.value) &&
+        variantsCopy[index].options.push({ name: e.target.value });
 
       resetCombinations();
 
-      e.target.value = '';
-
+      e.target.value = "";
     }
-
-  }
+  };
 
   const removeVariantOption = (i, j) => {
-
     var variantsCopy = [...variants];
 
     variantsCopy[i].options.splice(j, 1);
@@ -81,133 +97,163 @@ export default props => {
     setVariants(variantsCopy);
 
     resetCombinations();
-
-  }
+  };
 
   const resetCombinations = () => {
-
     var combs = [];
 
-    if(variants[2]) {
-
+    if (variants[2]) {
       variants[0].options.forEach(option0 =>
-
         variants[1].options.forEach(option1 =>
-
           variants[2].options.forEach(option2 =>
-
             combs.push({
               option_values: [option0, option1, option2].map(x => x.name),
-              price: 0.00,
+              price: 0.0
             })
-
           )
-
         )
-
-      )
-
-    }
-
-    else if(variants[1]) {
-
+      );
+    } else if (variants[1]) {
       variants[0].options.forEach(option0 =>
-
         variants[1].options.forEach(option1 =>
-
           combs.push({
             option_values: [option0, option1].map(x => x.name),
-            price: 0.00,
+            price: 0.0
           })
-
         )
-
-      )
-
-    }
-
-    else if(variants[0]) {
-
+      );
+    } else if (variants[0]) {
       variants[0].options.forEach(option =>
-
         combs.push({
           option_values: [option.name],
-          price: 0.00,
+          price: 0.0
         })
-
-      )
-
+      );
     }
 
     setCombinations(combs);
-
-  }
+  };
 
   const editVariantPrice = (index, e) => {
-
     var combinationsCopy = [...combinations];
 
     combinationsCopy[index].price = e.target.value;
 
     setCombinations(combinationsCopy);
+  };
 
-  }
+  useEffect(() => {
+    if (props.match.params.product_id !== "new") {
+      getProduct();
+    }
+  }, []);
 
   const createProduct = () => {
-
     var body = {
       name,
       description,
       price,
       variants,
-      combinations,
-    }
+      combinations
+    };
 
-    ApiClient.post('@store/products', body)
-    .then(res => {
+    setIsLoading(true);
 
-    })
-    .catch(console.log);
+    ApiClient.post("@store/products", body)
+      .then(res => {
+        const { success, error } = res;
 
-  }
+        if (success) {
+          window.location.href = "/products";
+        } else {
+          setIsLoading(false);
+
+          toast.error(error, {
+            position: toast.POSITION.TOP_CENTER
+          });
+        }
+      })
+      .catch(console.log);
+  };
 
   const updateProduct = () => {
-
     var body = {
       name,
       description,
       price,
       variants,
-      combinations,
-    }
+      combinations
+    };
 
-    ApiClient.put('@store/products', this.state.params)
-    .then(res => {
+    ApiClient.put("@store/products", this.state.params)
+      .then(res => {})
+      .catch(console.log);
+  };
 
-    })
-    .catch(console.log);
+  const getProduct = () => {
+    var product_id = props.match.params.product_id;
 
-  }
+    ApiClient.get(`@store/products/${product_id}`)
+      .then(res => {
+        const { success, product } = res;
+
+        setName(product.name);
+        setDescription(product.description);
+        setPrice(product.price);
+
+        var combinations = product.variants;
+
+        var variants = product.variant_types;
+
+        variants = variants.map(variant => {
+          variant.type = { id: variant.id, name: variant.name };
+          delete variant.id;
+          delete variant.name;
+          variant.options = variant.variant_options;
+          delete variant.variantOptions;
+          return variant;
+        });
+
+        setVariants(variants);
+
+        var options = variants.map(variant => variant.options).flat();
+
+        combinations = combinations.map(combination => {
+          var option_values = combination.options
+            .split(", ")
+            .map(x => x.split("_")[1]);
+
+          option_values = option_values.map(
+            x => options.filter(option => option.id == x)[0].name
+          );
+
+          combination.option_values = option_values;
+
+          return combination;
+        });
+
+        setCombinations(combinations);
+      })
+      .catch(console.log);
+  };
 
   const getBadgeColor = idx => {
-
-    const colors = ['outline-primary', 'outline-success', 'outline-danger'];
+    const colors = ["outline-primary", "outline-success", "outline-danger"];
 
     switch (idx) {
       case 0:
-        return colors[0]
+        return colors[0];
       case 1:
-        return colors[1]
+        return colors[1];
       case 2:
-        return colors[2]
+        return colors[2];
       default:
         return colors[Math.floor(Math.random() * colors.length)];
     }
-  }
+  };
 
   const getOptionType = () => {
-
-    const types = ['Size', 'Type', 'Sauce'];
+    const types = ["Size", "Type", "Sauce"];
 
     switch (variants.length) {
       case 0:
@@ -219,81 +265,134 @@ export default props => {
       default:
         return types[0];
     }
+  };
 
-  }
-
-  const variantsMarkup = variants.length && variants.map((variant, i) => (
-    <Row key={i} xs="12" md="9" style={{ marginTop: "1vh", marginBottom: "1vh" }}>
-      <Col>
-        <Input value={variant.type.name} onChange={e => editVariantType(e, i)} />
-      </Col>
-      <Col>
-        <Row style={{border: '1px solid #E4E7EA', borderRadius: 5, justifyContent: 'flex-start'}}>
-          {variant.options && variant.options.map((option, j) => (
-            <Button key={j} color={getBadgeColor(i)} onClick={() => removeVariantOption(i, j)} style={{margin: 5, position: 'relative'}}>
-              {option.name}
-            </Button>
-          ))}
-          <Input type='text' onKeyDown={e => e.key === 'Enter' && addVariantOption(e, i)} className='variant-option-input' />
-        </Row>
-      </Col>
-      <Col>
-        <Button onClick={() => removeVariant(variant)} color="danger">
-          Delete
-        </Button>
-      </Col>
-    </Row>
-  ));
+  const variantsMarkup =
+    variants.length &&
+    variants.map((variant, i) => (
+      <Row
+        key={i}
+        xs="12"
+        md="9"
+        style={{ marginTop: "1vh", marginBottom: "1vh" }}
+      >
+        <Col>
+          <Input
+            value={variant.type.name}
+            onChange={e => editVariantType(e, i)}
+          />
+        </Col>
+        <Col>
+          <Row
+            style={{
+              border: "1px solid #E4E7EA",
+              borderRadius: 5,
+              justifyContent: "flex-start"
+            }}
+          >
+            {variant.options &&
+              variant.options.map((option, j) => (
+                <Button
+                  key={j}
+                  color={getBadgeColor(i)}
+                  onClick={() => removeVariantOption(i, j)}
+                  style={{ margin: 5, position: "relative" }}
+                >
+                  {option.name}
+                </Button>
+              ))}
+            <Input
+              type="text"
+              onKeyDown={e => e.key === "Enter" && addVariantOption(e, i)}
+              className="variant-option-input"
+            />
+          </Row>
+        </Col>
+        <Col>
+          <Button onClick={() => removeVariant(variant)} color="danger">
+            Delete
+          </Button>
+        </Col>
+      </Row>
+    ));
 
   const addVariantMarkup = (
     <>
-      {
-        variants.length
-        ?
+      {variants.length ? (
         <>
           <Row>
-            <Col><strong>Option Type</strong></Col>
-            <Col><strong>Option Value</strong></Col>
+            <Col>
+              <strong>Option Type</strong>
+            </Col>
+            <Col>
+              <strong>Option Value</strong>
+            </Col>
             <Col />
           </Row>
           {variantsMarkup}
-          <br/>
-          {variants.length < 3 && <p style={{color: '#6A84F0', cursor: 'pointer'}} onClick={addVariant}>Add more option type</p>}
+          <br />
+          {variants.length < 3 && (
+            <p
+              style={{ color: "#6A84F0", cursor: "pointer" }}
+              onClick={addVariant}
+            >
+              Add more option type
+            </p>
+          )}
         </>
-        :
+      ) : (
         <>
-          <p>Add variants if this product comes in multiple versions, like different sizes or types.</p>
+          <p>
+            Add variants if this product comes in multiple versions, like
+            different sizes or types.
+          </p>
           <Button onClick={addVariant}>Add variants</Button>
         </>
-      }
+      )}
     </>
-  )
+  );
 
-  const variantCombinationMarkup = (
-    combinations.length > 0 && combinations.map((combination, i) => (
-      <Row key={i} style={{marginTop: '1vh', marginBottom: '1vh'}}>
-        <Col>
-          <Button color='outline-dark'>{combination.option_values.join(' · ')}</Button>
-        </Col>
-        <Col>
-          <Input type='number' step='1' min='0' placeholder='price' value={combination.price} onChange={e => editVariantPrice(i, e)} />
-        </Col>
-        <Col />
-      </Row>
-    ))
-  )
-
-  const editVariantMarkup = (
+  const variantCombinationMarkup =
     combinations.length > 0 &&
+    combinations.map((combination, i) => (
+      <Row key={i} style={{ marginTop: "1vh", marginBottom: "1vh" }}>
+        <Col>
+          <Button color="outline-dark">
+            {combination.option_values.join(" · ")}
+          </Button>
+        </Col>
+        <Col>
+          <Input
+            type="number"
+            step="1"
+            min="0"
+            placeholder="price"
+            value={combination.price}
+            onChange={e => editVariantPrice(i, e)}
+          />
+        </Col>
+        <Col>
+          {props.match.params.product_id !== "new" && (
+            <i className="icon-pencil" />
+          )}
+        </Col>
+      </Row>
+    ));
+
+  const editVariantMarkup = combinations.length > 0 && (
     <>
       <Row>
-        <Col><strong>Variant</strong></Col>
-        <Col><strong>Price</strong></Col>
+        <Col>
+          <strong>Variant</strong>
+        </Col>
+        <Col>
+          <strong>Price</strong>
+        </Col>
         <Col />
       </Row>
       {variantCombinationMarkup}
     </>
-  )
+  );
 
   return (
     <div className="animated fadeIn">
@@ -303,26 +402,45 @@ export default props => {
         </CardHeader>
 
         <CardBody>
-          <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
+          <Form
+            action=""
+            method="post"
+            encType="multipart/form-data"
+            className="form-horizontal"
+          >
             <FormGroup>
               <div>
                 <Row md="3">
-                  <Col md="4"><strong>Title *</strong></Col>
-                  {
-                    variants.length <= 0 &&
-                    <Col md="4"><strong>Price (RM) *</strong></Col>
-                  }
+                  <Col md="4">
+                    <strong>Title *</strong>
+                  </Col>
+                  {variants.length <= 0 && (
+                    <Col md="4">
+                      <strong>Price (RM) *</strong>
+                    </Col>
+                  )}
                 </Row>
                 <Row md="3">
                   <Col md="4">
-                    <Input type="text" placeholder="French Toast" onChange={e => setName(e.target.value)} />
+                    <Input
+                      type="text"
+                      value={name}
+                      placeholder="French Toast"
+                      onChange={e => setName(e.target.value)}
+                    />
                   </Col>
-                  {
-                    variants.length <= 0 &&
+                  {variants.length <= 0 && (
                     <Col md="4">
-                      <Input type="number" step="1" min="0" placeholder="15.00" onChange={e => setPrice(e.target.value)} />
+                      <Input
+                        value={price}
+                        type="number"
+                        step="1"
+                        min="0"
+                        placeholder="15.00"
+                        onChange={e => setPrice(e.target.value)}
+                      />
                     </Col>
-                  }
+                  )}
                 </Row>
               </div>
             </FormGroup>
@@ -330,11 +448,19 @@ export default props => {
             <FormGroup>
               <div>
                 <Row md="3">
-                  <Col><strong>Description</strong></Col>
+                  <Col>
+                    <strong>Description</strong>
+                  </Col>
                 </Row>
                 <Row xs="12" md="9">
                   <Col>
-                    <Input type="textarea" rows="9" placeholder="Texas Toast batter dipped, grilled to a golden brown and dusted with powdered sugar!" onChange={e => setDescription(e.target.value)} />
+                    <Input
+                      type="textarea"
+                      value={description}
+                      rows="9"
+                      placeholder="Texas Toast batter dipped, grilled to a golden brown and dusted with powdered sugar!"
+                      onChange={e => setDescription(e.target.value)}
+                    />
                   </Col>
                 </Row>
               </div>
@@ -343,18 +469,24 @@ export default props => {
             <FormGroup>
               <div>
                 <Row md="3">
-                  <Col><strong>Images</strong></Col>
+                  <Col>
+                    <strong>Images</strong>
+                  </Col>
                 </Row>
                 <Row xs="12" md="9">
                   <Col>
-                    <Input type="file" id="file-multiple-input" name="file-multiple-input" multiple />
+                    <Input
+                      type="file"
+                      id="file-multiple-input"
+                      name="file-multiple-input"
+                      multiple
+                    />
                   </Col>
                 </Row>
               </div>
             </FormGroup>
           </Form>
         </CardBody>
-
       </Card>
 
       <Card>
@@ -365,18 +497,20 @@ export default props => {
         <CardBody>
           <FormGroup>
             <div>
-              {addVariantMarkup}
+              {props.match.params.product_id === "new" && addVariantMarkup}
               {editVariantMarkup}
             </div>
           </FormGroup>
         </CardBody>
 
         <CardFooter>
-          <Button type="submit" size="sm" color="primary" style={{marginRight: 20}}><i className="fa fa-dot-circle-o"></i> Submit</Button>
-          <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+          <LoadingButton
+            isLoading={isLoading}
+            text="Save"
+            onClick={createProduct}
+          />
         </CardFooter>
       </Card>
-
     </div>
   );
-}
+};
--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,140 +1,149 @@
-import React, { Component } from 'react';
-import { Button, Badge, Card, CardBody, CardFooter, Col, Row, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
-import { AppSwitch } from '@coreui/react'
-import ApiClient from '../../ApiClient';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Button,
+  Badge,
+  Card,
+  CardBody,
+  CardFooter,
+  Col,
+  Row,
+  Pagination,
+  PaginationItem,
+  PaginationLink
+} from "reactstrap";
+import { AppSwitch } from "@coreui/react";
+import ApiClient from "../../ApiClient";
+import { Link } from "react-router-dom";
 
 class Products extends Component {
-
   constructor(props) {
-
     super(props);
 
     this.listProducts = this.listProducts.bind(this);
 
     this.state = {
-      products: this.generateProducts(),
-      totalItems: 30,
-      currentPage: 1,
+      products: [],
+      lastPage: 1,
+      currentPage: 1
     };
-
-  }
-
-  generateProducts() {
-    var products = [];
-    var names = [
-      "Big breakfast",
-      "Small breakfast",
-      "Lunch Set A",
-      "Breakfast Set B",
-    ];
-    var images = [
-      "https://tinyurl.com/rfqgjsb",
-      "https://tinyurl.com/wg537ey",
-      "https://tinyurl.com/rrscpnw",
-      "https://tinyurl.com/qk72g7x",
-    ];
-    var types = [
-      "Food",
-      "Beverage",
-      "Dessert",
-    ];
-
-    for (let i = 0; i < 20; i++) {
-      var table = {
-        id: i,
-        name: names[Math.floor(Math.random() * names.length)],
-        type: types[Math.floor(Math.random() * types.length)],
-        image: images[Math.floor(Math.random() * images.length)],
-        price: Math.floor(Math.random() * 30 + 10),
-      }
-
-      products.push(table);
-    }
-
-    return products;
   }
 
   componentDidMount() {
-
-    // this.listProducts();
-
+    this.listProducts();
   }
 
   listProducts() {
-
-    ApiClient.get('@store/products')
+    ApiClient.get("@store/products")
       .then(res => {
+        const { success, products } = res;
 
-        const { success, products, totalItems, currentPage } = res;
+        console.log(products.data);
 
         if (success) {
-
           this.setState({
-            products,
-            totalItems,
-            currentPage
+            products: products.data,
+            lastPage: products.last_page,
+            currentPage: products.current_page
           });
-
         } else {
-
           // TODO: show error
-
         }
-
       })
       .catch(console.log);
-
   }
 
   render() {
-
-    const { products, totalItems, currentPage } = this.state;
+    const { products, lastPage, currentPage } = this.state;
 
     const viewCreateProductPage = "/products/new";
 
-    const viewUpdateProductPage = (productId) => "/products/" + productId;
+    const viewUpdateProductPage = productId => "/products/" + productId;
 
-    const productsMarkup = products.length > 0 && products.map((product, index) => (
-      <Col xs="12" sm="6" md="2">
-        <Link to={viewUpdateProductPage(product.id)} style={{ textDecoration: 'none', color: 'black' }}>
-          <Card>
-            <CardBody style={styles.productCard}>
-              <Badge color="danger" style={styles.badge}>Hot Item</Badge>
-              <div style={{ height: 'inherit', width: 'inherit', backgroundSize: 'cover', backgroundImage: `url(${product.image})` }}>
-              </div>
-            </CardBody>
-            <CardFooter style={{ textAlign: 'center' }}>
-              <b><p>{product.name}</p></b>
-              <p>{'RM ' + product.price}</p>
-            </CardFooter>
-          </Card>
-        </Link>
-      </Col>
-    ));
+    const productsMarkup =
+      products.length > 0 &&
+      products.map((product, index) => (
+        <Col xs="12" sm="6" md="2">
+          <Link
+            to={viewUpdateProductPage(product.id)}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <Card>
+              <CardBody style={styles.productCard}>
+                {product.tag && (
+                  <Badge color="danger" style={styles.badge}>
+                    {product.tag}
+                  </Badge>
+                )}
+                <div
+                  style={{
+                    height: "inherit",
+                    width: "inherit",
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${
+                      product.images.length
+                        ? product.images[0].url
+                        : "https://tinyurl.com/wgr44k7"
+                    })`
+                  }}
+                ></div>
+              </CardBody>
+              <CardFooter style={{ textAlign: "center" }}>
+                <b>
+                  <p>{product.name}</p>
+                </b>
+                <p>{"RM " + product.price}</p>
+              </CardFooter>
+            </Card>
+          </Link>
+        </Col>
+      ));
 
-    var paginationMarkup = [];
+    // var paginationMarkup = [];
 
-    for (var i = 1; i <= (totalItems / 18) + 1; i++) {
-      paginationMarkup.push(
-        <PaginationItem active={i == currentPage ? true : false}>
-          <PaginationLink tag="button">{i}</PaginationLink>
+    // for (var i = 1; i <= (totalItems / 18) + 1; i++) {
+    //   paginationMarkup.push(
+    //     <PaginationItem active={i === currentPage ? true : false}>
+    //       <PaginationLink tag="button">{i}</PaginationLink>
+    //     </PaginationItem>
+    //   );
+    // }
+
+    var paginationMarkup = [
+      this.state.currentPage > 1 && (
+        <PaginationItem active={false}>
+          <PaginationLink tag="button">
+            {this.state.currentPage - 1}
+          </PaginationLink>
         </PaginationItem>
-      );
-    }
+      ),
+      <PaginationItem active={true}>
+        <PaginationLink tag="button">{this.state.currentPage}</PaginationLink>
+      </PaginationItem>,
+      <PaginationItem active={false}>
+        <PaginationLink tag="button">
+          {this.state.currentPage + 1}
+        </PaginationLink>
+      </PaginationItem>
+    ];
 
     return (
       <div className="animated fadeIn">
         <Col>
-          <Row style={{ justifyContent: 'flex-end', marginBottom: "3vh", marginRight: "0.2vw" }}>
+          <Row
+            style={{
+              justifyContent: "flex-end",
+              marginBottom: "3vh",
+              marginRight: "0.2vw"
+            }}
+          >
             <Link to={viewCreateProductPage}>
               <Button color="primary">Add product</Button>
             </Link>
           </Row>
-          <Row>
-            {productsMarkup}
-          </Row>
-          <Pagination style={{ position: 'absolute', bottom: 25, right: 5 }}>
+          <Row>{productsMarkup}</Row>
+        </Col>
+        {this.state.products.length > 0 && (
+          <Pagination style={{ position: "absolute", bottom: 50, right: 20 }}>
             <PaginationItem>
               <PaginationLink previous tag="button"></PaginationLink>
             </PaginationItem>
@@ -143,8 +152,7 @@ class Products extends Component {
               <PaginationLink next tag="button"></PaginationLink>
             </PaginationItem>
           </Pagination>
-
-        </Col>
+        )}
       </div>
     );
   }
@@ -152,14 +160,14 @@ class Products extends Component {
 
 const styles = {
   badge: {
-    position: 'absolute',
+    position: "absolute",
     top: 10,
     right: 10
   },
   productCard: {
-    height: '20vh',
+    height: "20vh",
     padding: 0
   }
-}
+};
 
 export default Products;
